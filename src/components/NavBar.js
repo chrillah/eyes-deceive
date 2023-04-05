a5e745ed23857b4601b182174607fc05081e0958
chrillah <ch.ronnberg@gmail.com>
2023-04-05 13:53:55 +0200

    icons added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,46 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import MenuLinks from './MenuLinks'
 
 function NavBar() {
-  return (
-    <>
-    <nav className="nav-bar">
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/crazy-corner">CrazyCorner</Link>
-            </li>
-        </ul>
-        {/* <div className="logo-container"></div>
+    return (
+        <>
+            <nav className="nav-bar">
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <svg
+                            className='home-eye'
+                                id="Layer_1"
+                                data-name="Layer 1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 50 30"
+                            >
+                                <polygon
+                                    id="triangle"
+                                    points="50 30 25 0 0 30 50 30"
+                                />
+                                <path
+                                    id="white"
+                                    class="home-eye-white"
+                                    d="M15.33,10.33h0c10.68,0,19.34,8.67,19.34,19.34h0c-10.68,0-19.34-8.67-19.34-19.34h0Z"
+                                    transform="translate(-6.82 23.54) rotate(-45)"
+                                />
+                                <circle className='home-eye-pupil' id="eye" cx="25" cy="20" r="4.59" />
+                            </svg>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/crazy-corner">CrazyCorner</Link>
+                    </li>
+                </ul>
+                {/* <div className="logo-container"></div>
         <div className="hamburger-btn">
         </div> */}
-        {/* <MenuLinks /> */}
-    </nav>
-    </>
-  )
+                {/* <MenuLinks /> */}
+            </nav>
+        </>
+    )
 }
 
 export default NavBar

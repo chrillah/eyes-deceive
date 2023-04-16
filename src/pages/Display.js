a7c8f3b88eb170af5873fdd5747a5ca21356dfcc
chrillah <ch.ronnberg@gmail.com>
2023-04-16 02:40:41 +0200

    function added

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -1,13 +1,131 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Display() {
     const { style } = useParams()
+    function arrow() {
+        if (style === 'bg-pattern-triangles') {
+            return true
+        }
+        if (style === 'bg-pattern-triangles bg-animated-triangles') {
+            return true
+        }
+    }
+    function circle() {
+        if (style === 'bg-color-circle') {
+            return true
+        }
+        if (style === 'bg-color-animated-circle') {
+            return true
+        }
+    }
+    function kors() {
+        if (style === 'bg-pattern-grid') {
+            return true
+        }
+        if (style === 'bg-pattern-grid bg-animated-grid') {
+            return true
+        }
+    }
+    function quick() {
+        if (style === 'bg-gradient-1') {
+            return true
+        }
+        if (style === 'bg-gradient-1 bg-animated-gradient-1') {
+            return true
+        }
+    }
+    function crossBtn() {
+        if (style === 'bg-pattern-cross') {
+            return true
+        }
+        if (style === 'bg-pattern-cross bg-animated-cross') {
+            return true
+        }
+    }
+    function dotsBtn() {
+        if (style === 'bg-pattern-dots') {
+            return true
+        }
+        if (style === 'bg-pattern-dots bg-animated-dots') {
+            return true
+        }
+    }
+    function zigBtn() {
+        if (style === 'bg-pattern-zig-zag') {
+            return true
+        }
+        if (style === 'bg-pattern-zig-zag bg-animated-zig-zag') {
+            return true
+        }
+    }
+    function waveBtn() {
+        if (style === 'bg-pattern-wave') {
+            return true
+        }
+        if (style === 'bg-pattern-wave bg-animated-wave') {
+            return true
+        }
+    }
+    function circlesBtn() {
+        if (style === 'bg-pattern-circles') {
+            return true
+        }
+        if (style === 'bg-pattern-circles bg-animated-circles') {
+            return true
+        }
+    }
     return (
         <div className={style}>
             <div className="display-wrapper">
                 <div className="display-container">
-                    {style === 'bg-pattern-cross bg-animated-cross' ? (
+                    {arrow() ? (
+                        <div className="grid-triangle-1">
+                            <svg
+                                className={
+                                    style === 'bg-pattern-triangles'
+                                        ? 'black-arrow'
+                                        : 'black-arrow black-arrow-spin'
+                                }
+                                id="Layer_1"
+                                data-name="Layer 1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 55.00 55.00"
+                            >
+                                <polyline
+                                    id="POINT"
+                                    className="black-arrow-details"
+                                    points="23.82 .35 .71 23.46 23.82 46.57"
+                                />
+                                <line
+                                    id="LINE"
+                                    className="black-arrow-details"
+                                    x1=".71"
+                                    y1="23.46"
+                                    x2="46"
+                                    y2="23.46"
+                                />
+                            </svg>
+                        </div>
+                    ) : (
+                        ''
+                    )}
+                    {circle() ? (
+                        <div className="grid-color-1">
+                            <div className="grid-inner-color-1" />
+                        </div>
+                    ) : (
+                        ''
+                    )}
+                    {quick() ? (
+                        <h1 className="grid-pattern-header-1">ℚ⌰⟟⍧⏧ ⍓⍲⍑ℍ⎎</h1>
+                    ) : (
+                        ''
+                    )}
+                    {kors() ?                                     <h1 className="grid-grid-header">
+                                        🜍
+                                    </h1> : ''}
+                    {crossBtn() ? (
                         <a
                             href="https://www.youtube.com/watch?v=pIvTdPBFY6I&ab_channel=FadetoMind"
                             target="_blanc"
@@ -18,7 +136,7 @@ function Display() {
                     ) : (
                         ''
                     )}
-                    {style === 'bg-pattern-dots bg-animated-dots'  ? (
+                    {dotsBtn() ? (
                         <a
                             href="https://www.youtube.com/watch?v=kSo6LT0xPSQ&ab_channel=Windows96-Topic"
                             target="_blanc"
@@ -29,7 +147,7 @@ function Display() {
                     ) : (
                         ''
                     )}
-                    {style === 'bg-pattern-zig-zag bg-animated-zig-zag' ? (
+                    {zigBtn() ? (
                         <a
                             href="https://www.youtube.com/watch?v=CEaRON-XP3g&ab_channel=Vaperror-Topic"
                             target="_blanc"
@@ -40,8 +158,8 @@ function Display() {
                     ) : (
                         ''
                     )}
-                    {style === 'bg-pattern-wave bg-animated-wave' ? (
-                            <a
+                    {waveBtn() ? (
+                        <a
                             href="https://www.youtube.com/watch?v=Xmx0uNbqPZY&ab_channel=ElectronicGems"
                             target="_blanc"
                             className="wave-btn"
@@ -51,14 +169,14 @@ function Display() {
                     ) : (
                         ''
                     )}
-                    {style === 'bg-pattern-circles bg-animated-circles' ? (
-                              <a
-                              href="https://www.youtube.com/watch?v=fHiO2HTGUcA&ab_channel=tswenner"
-                              target="_blanc"
-                              className='circle-btn'
-                          >
-                             🜯
-                          </a>
+                    {circlesBtn() ? (
+                        <a
+                            href="https://www.youtube.com/watch?v=fHiO2HTGUcA&ab_channel=tswenner"
+                            target="_blanc"
+                            className="circle-btn"
+                        >
+                            🜯
+                        </a>
                     ) : (
                         ''
                     )}
